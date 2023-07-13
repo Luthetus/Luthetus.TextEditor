24b658c74fc63c654173c3dafcd3bfbd8245f78e
Hunter Freeman <luthetus@gmail.com>
2023-07-13 17:20:33 -0400

    TextEditorJsonCompilerService

---
+++ Source/Lib/Luthetus.TextEditor.RazorLib/CompilerServiceCase/JsonCase/JsonResource.cs
@@ -0,0 +1,23 @@
+﻿using Luthetus.TextEditor.RazorLib.Lexing;
+using Luthetus.TextEditor.RazorLib.Model;
+using System.Collections.Immutable;
+
+namespace Luthetus.TextEditor.RazorLib.CompilerServiceCase.JsonCase;
+
+public class JsonResource
+{
+    public JsonResource(
+        TextEditorModelKey modelKey,
+        ResourceUri resourceUri,
+        TextEditorJsonCompilerService textEditorJsonCompilerService)
+    {
+        ModelKey = modelKey;
+        ResourceUri = resourceUri;
+        TextEditorJsonCompilerService = textEditorJsonCompilerService;
+    }
+
+    public TextEditorModelKey ModelKey { get; }
+    public ResourceUri ResourceUri { get; }
+    public TextEditorJsonCompilerService TextEditorJsonCompilerService { get; }
+    public ImmutableArray<TextEditorTextSpan>? SyntacticTextSpans { get; internal set; }
+}
---
+++ Source/Lib/Luthetus.TextEditor.RazorLib/CompilerServiceCase/JsonCase/TextEditorJsonCompilerService.cs
@@ -0,0 +1,105 @@
+﻿using Luthetus.Common.RazorLib.BackgroundTaskCase.BaseTypes;
+using Luthetus.TextEditor.RazorLib.Analysis.Json.SyntaxActors;
+using Luthetus.TextEditor.RazorLib.Analysis.Html.SyntaxActors;
+using Luthetus.TextEditor.RazorLib.HostedServiceCase.CompilerServiceCase;
+using Luthetus.TextEditor.RazorLib.Lexing;
+using Luthetus.TextEditor.RazorLib.Model;
+using System.Collections.Immutable;
+using System.Reflection;
+
+namespace Luthetus.TextEditor.RazorLib.CompilerServiceCase.JsonCase;
+
+public class TextEditorJsonCompilerService : ICompilerService
+{
+    private readonly Dictionary<TextEditorModelKey, JsonResource> _cssResourceMap = new();
+    private readonly object _cssResourceMapLock = new();
+    private readonly ICompilerServiceBackgroundTaskQueue _compilerServiceBackgroundTaskQueue;
+
+    public TextEditorJsonCompilerService(ICompilerServiceBackgroundTaskQueue compilerServiceBackgroundTaskQueue)
+    {
+        _compilerServiceBackgroundTaskQueue = compilerServiceBackgroundTaskQueue;
+    }
+
+    public void RegisterModel(TextEditorModel model)
+    {
+        lock (_cssResourceMapLock)
+        {
+            if (_cssResourceMap.ContainsKey(model.ModelKey))
+                return;
+
+            _cssResourceMap.Add(
+                model.ModelKey,
+                new(model.ModelKey, model.ResourceUri, this));
+
+            QueueParseRequest(model);
+        }
+    }
+
+    public ImmutableArray<TextEditorTextSpan> GetSyntacticTextSpansFor(TextEditorModel model)
+    {
+        lock (_cssResourceMapLock)
+        {
+            if (!_cssResourceMap.ContainsKey(model.ModelKey))
+                return ImmutableArray<TextEditorTextSpan>.Empty;
+
+            return _cssResourceMap[model.ModelKey].SyntacticTextSpans ??
+                ImmutableArray<TextEditorTextSpan>.Empty;
+        }
+    }
+
+    public ImmutableArray<ITextEditorSymbol> GetSymbolsFor(TextEditorModel textEditorModel)
+    {
+        return ImmutableArray<ITextEditorSymbol>.Empty;
+    }
+
+    public ImmutableArray<TextEditorTextSpan> GetDiagnosticTextSpansFor(TextEditorModel textEditorModel)
+    {
+        return ImmutableArray<TextEditorTextSpan>.Empty;
+    }
+
+    public void ModelWasModified(TextEditorModel textEditorModel, ImmutableArray<TextEditorTextSpan> editTextSpans)
+    {
+        // Do nothing
+    }
+
+    public void DisposeModel(TextEditorModel textEditorModel)
+    {
+        lock (_cssResourceMapLock)
+        {
+            _cssResourceMap.Remove(textEditorModel.ModelKey);
+        }
+    }
+
+    private void QueueParseRequest(TextEditorModel model)
+    {
+        var parseBackgroundWorkItem = new BackgroundTask(
+            async cancellationToken =>
+            {
+                var text = model.GetAllText();
+
+                var lexer = new TextEditorJsonLexer(model.ResourceUri);
+                var lexResult = await lexer.Lex(text, model.RenderStateKey);
+
+                lock (_cssResourceMapLock)
+                {
+                    if (!_cssResourceMap.ContainsKey(model.ModelKey))
+                        return;
+
+                    _cssResourceMap[model.ModelKey]
+                        .SyntacticTextSpans = lexResult;
+                }
+
+                await model.ApplySyntaxHighlightingAsync();
+
+                return;
+            },
+            "TODO: name",
+            "TODO: description",
+            false,
+            _ => Task.CompletedTask,
+            null,
+            CancellationToken.None);
+
+        _compilerServiceBackgroundTaskQueue.QueueBackgroundWorkItem(parseBackgroundWorkItem);
+    }
+}
